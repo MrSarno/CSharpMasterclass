c05871f2b170886083a62af79b9aab40934e0c81
MrSarno <email@sarno.dev>
2022-09-14 15:11:23 +0100

    fix: 002 not properly pushed

--- 002-VariablesAndDataTypes/Program.cs
+++ 002-VariablesAndDataTypes/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myFirstNumber = 2;
+            float firstPiDigits = 3.1415f;
+            bool isNumber = false;
+            string myFirstText = "Hi mom!";
+            char yesChar = 'y';
+
+
+            Console.WriteLine(myFirstNumber);
+            Console.WriteLine(firstPiDigits);
+            Console.WriteLine(isNumber);
+            Console.WriteLine(myFirstText);
+            Console.WriteLine(yesChar);
+        }
+    }
+}
