8ac5643f3b59095d456466e58a03d04536d9d684
MrSarno <email@sarno.dev>
2022-09-16 19:52:55 +0100

    chore: add 006

---
+++ 006-Comments/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SomeBestPractices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // This is a single-line comment.
+
+            /*
+             * This is a multi-line comment.
+             */
+
+            /// <summary>
+            /// An XML documentation comment.
+            /// </summary>
+
+        }
+    }
+}
