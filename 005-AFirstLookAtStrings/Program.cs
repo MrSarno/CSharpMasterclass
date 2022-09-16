49a1183d06f057b69a470dd64a4dbb00644f81ce
MrSarno <email@sarno.dev>
2022-09-16 17:23:55 +0100

    chore: add 005

---
+++ 005-AFirstLookAtStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AFirstLookAtStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myFirstName = "John";
+            string printMyFirstName = "My name is " + myFirstName + ".";
+            Console.WriteLine(printMyFirstName);
+            Console.WriteLine("\n");
+
+            string printANewSentence = "This sentence will be displayed with various different capitalisations.";
+            Console.WriteLine(printANewSentence);
+            Console.WriteLine(printANewSentence.ToUpper());
+            Console.WriteLine(printANewSentence.ToLower());
+
+            string printTheSameSentenceInAllCaps = printANewSentence.ToUpper();
+            Console.WriteLine(printTheSameSentence);
+
+            Console.ReadLine();
+        }
+    }
+}
