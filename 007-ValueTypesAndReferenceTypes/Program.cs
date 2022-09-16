241b72339946d339a59dbee9d858d1740d0284ca
MrSarno <email@sarno.dev>
2022-09-16 20:06:25 +0100

    chore: add 007

---
+++ 007-ValueTypesAndReferenceTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ValueTypesAndReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A value type holds data directly in its own memory space. It uses a heap to store the value.\n");
+            Console.WriteLine("Value types include: int, float, long, double, char, bool, decimal, and double.\n\n");
+            Console.WriteLine("Reference types instead store the memory location of the actual data.\n");
+            Console.WriteLine("Reference types include: string, class, array.");
+            Console.ReadLine();
+        }
+    }
+}
