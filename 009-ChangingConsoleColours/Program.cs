ee5dc3e696600b5238e3c1dfe04895aee7636adb
MrSarno <email@sarno.dev>
2022-09-23 23:46:09 +0100

    chore: add 009

---
+++ 009-ChangingConsoleColours/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChangingConsoleColours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+
+            Console.Clear(); // Using this to apply the background colour to
+            // the entire console window, not just behind the foreground text.
+
+            Console.WriteLine("Hello, colour palette!");
+            Console.ReadLine();
+        }
+    }
+}
