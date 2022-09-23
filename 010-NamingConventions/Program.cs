9f18ff169ff34389e5f42f11d662bab518a5c1b6
MrSarno <email@sarno.dev>
2022-09-23 23:54:02 +0100

    chore: add 010

---
+++ 010-NamingConventions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NamingConventions
+{
+    class Program // Pascal case: e.g. ClientActivity
+    {
+        static void Main(string[] args) // Pascal case: e.g. MyMethodName
+        {
+
+            int myAge = 30; // Camel case
+
+            // Use full words; e.g. userControl; not UsrCtr
+
+            // Don't use numbers at the start of variable names
+
+            // Avoid capitalising variable types, e.g. int, not Int
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("\nI am currently " + myAge + " years old.");
+            Console.ReadLine();
+
+            // https://www.dofactory.com/csharp-coding-standards is a great reference
+        }
+    }
+}
