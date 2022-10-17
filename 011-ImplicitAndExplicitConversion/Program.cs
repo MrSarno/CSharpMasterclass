d943490d9fbac0b35300c35d47bf0a350127d89c
MrSarno <email@sarno.dev>
2022-10-17 23:49:03 +0100

    chore: add 011

---
+++ 011-ImplicitAndExplicitConversion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ImplicitAndExplicitConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double myDouble = 13.37;
+            int myInt;
+
+            myInt = (int)myDouble; // An example of explicit conversion
+            Console.WriteLine("My int is " + myInt + ".");
+            //Console.Read();
+
+            int myFirstBigNum = 12390532;
+            long mySecondBigNum = myFirstBigNum; // An example of implicit conversion
+
+            Console.WriteLine("\n\nMy next int is " + myFirstBigNum + ".");
+            Console.WriteLine("My long is " + mySecondBigNum + ".");
+
+            float myFloat = 13.37F;
+            double myNewDouble = myFloat;
+
+            Console.WriteLine("\n\nMy float is " + myFloat + ".");
+            Console.WriteLine("My long is " + myNewDouble + ".");
+
+            string myString = myNewDouble.ToString();
+            Console.WriteLine("\n\nMy string is '" + myString + "'.");
+
+            Console.Read();
+        }
+    }
+}
