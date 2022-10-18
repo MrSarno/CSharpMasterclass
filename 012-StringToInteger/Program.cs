a3a2c8cd745d115871d757ab2ed402eef13fbfca
MrSarno <email@sarno.dev>
2022-10-18 23:00:33 +0100

    chore: add 012

---
+++ 012-StringToInteger/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StringToInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myFirstString = "15";
+            string mySecondString = "13";
+
+            string myFirstResult = myFirstString + mySecondString;
+            Console.WriteLine("myFirstString + mySecondString = " + myFirstResult + ".");
+
+            string myThirdString = "15";
+            string myFourthString = "13";
+
+            int num1 = Int32.Parse(myThirdString);
+            int num2 = Int32.Parse(myFourthString);
+
+            int mySecondResult = num1 + num2;
+            Console.WriteLine("\n\nmyThirdString + myFourthString = " + mySecondResult + ".");
+
+            Console.ReadLine();
+        }
+    }
+}
