7614557046c80b304dc5448c7d8947d2a622967e
MrSarno <email@sarno.dev>
2022-10-25 19:44:02 +0100

    chore: add 016

---
+++ 016-FunctionsAndMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+
+namespace FunctionsAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A method is a code block that contains a series of statements.
+
+            // A program causes the statements to be executed by calling the method
+            // and specifying any required method arguments. In C#, every executed
+            // instruction is performed in the context of a method.
+
+            // The Main method is the entry point for every C# application and it's
+            // called by the common language runtime (CLR) when the program is started.
+
+            // https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/methods
+
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
