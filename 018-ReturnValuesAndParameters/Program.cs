97d0819eb2eea7f8a198a67ab8796e6aac6ab62c
MrSarno <email@sarno.dev>
2022-10-27 14:52:19 +0100

    chore: add 018

---
+++ 018-ReturnValuesAndParameters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ReturnValuesAndParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int result = Add(46, 72);
+            // Console.WriteLine(result);
+
+            Console.WriteLine(Add(46, 72));
+
+            Console.WriteLine(Add(Add(1, 2), Add(5, 6)));
+
+            Console.WriteLine(Multiply(5,10));
+
+            Console.WriteLine(Divide(4, 2));
+
+            Console.ReadLine();
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
