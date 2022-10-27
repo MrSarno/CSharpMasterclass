0fc427685e5b74f11a48481a69dca8466b4022df
MrSarno <email@sarno.dev>
2022-10-27 14:37:59 +0100

    chore: add 017

---
+++ 017-VoidMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace VoidMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteSomething();
+        }
+
+        public static void WriteSomething() // must be static as Main is static
+        {
+            Console.WriteLine("I am called from a method.");
+            Console.ReadLine();
+        }
+    }
+}
