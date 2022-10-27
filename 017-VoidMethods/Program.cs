749ef55299a7ebfc9da8c92eb3008473a4ea4091
MrSarno <email@sarno.dev>
2022-10-27 14:42:31 +0100

    chore: expand 017

--- 017-VoidMethods/Program.cs
+++ 017-VoidMethods/Program.cs
@@ -8,11 +8,17 @@ namespace VoidMethods
         static void Main(string[] args)
         {
             WriteSomething();
+            WriteSomethingSpecific("\nI am an argument and I am called from a method.");
         }
 
         public static void WriteSomething() // must be static as Main is static
         {
             Console.WriteLine("I am called from a method.");
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
             Console.ReadLine();
         }
     }
