65e2e1a84a9454bdc2cc253d59a7cb76578dded9
MrSarno <email@sarno.dev>
2022-11-03 21:33:02 +0000

    chore: add 029

---
+++ 029-WhileLoops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            // while(counter < 10)
+            // {
+            //     Console.WriteLine(counter);
+            //     counter++;
+            // }
+
+            string enteredText = "";
+
+            while(enteredText.Equals(""))
+            {
+                counter++;
+                Console.WriteLine("\nPlease press Enter if you wish to keep going.");
+                Console.WriteLine("Press any other button, then Enter, to stop.");
+                Console.WriteLine("Current count is: {0}.", counter);
+                enteredText = Console.ReadLine();
+            }
+        }
+    }
+}
