9ecdf1a29ec93c1db77bf2c79c690379c7f61fff
MrSarno <email@sarno.dev>
2022-11-03 21:10:02 +0000

    chore: add 027

---
+++ 027-ForLoops/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // LOOP ONE
+
+            Console.WriteLine("For loop #1 is about to begin.\n");
+
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i); // I am deliberately putting them on new lines
+            }
+
+            Console.WriteLine("\nFor loop #1 has finished.");
+
+            // LOOP TWO
+
+            Console.WriteLine("\n\nFor loop #2 is about to begin.\n");
+
+            for (int j = 10; j > 0; j--)
+            {
+                Console.WriteLine(j);
+            }
+
+            Console.WriteLine("\nFor loop #2 has finished.");
+
+            // LOOP THREE
+
+            Console.WriteLine("\n\nFor loop #3 is about to begin.\n");
+
+            for (int k = 0; k < 26; k+=5)
+            {
+                Console.WriteLine(k + " is lower than " + (k+5) +".");
+            }
+
+            Console.WriteLine("\nFor loop #3 has finished.");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
