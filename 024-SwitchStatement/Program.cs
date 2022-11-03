7c13a86b6d56be555e0596b1f5c632ffd6c540b3
MrSarno <email@sarno.dev>
2022-11-03 20:15:58 +0000

    chore: add 024

---
+++ 024-SwitchStatement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 25; // change variable value to explore different program behaviours
+
+            switch(myAge)
+            {
+                case 15:
+                    Console.WriteLine("Too young to be allowed entry.");
+                    Console.ReadLine();
+                    break;
+                case 25:
+                    Console.WriteLine("Good to go!");
+                    Console.ReadLine();
+                    break;
+                default:
+                    Console.WriteLine("How old are you, then?");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+    }
+}
