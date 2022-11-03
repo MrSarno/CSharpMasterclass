a21ac075ce0182debc0fda17c2b0aed6ad33826c
MrSarno <email@sarno.dev>
2022-11-03 20:56:50 +0000

    chore: add 026

---
+++ 026-Loops/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for loops
+            Console.WriteLine("FOR loops run a number of times that depend on the starting value and the increment.");
+
+            // while loops
+            Console.WriteLine("\nWHILE loops run a number of times that depend on the condition continuing to be met.");
+            Console.WriteLine("Ensure that you are incrementing within the loop!");
+
+            // do while loops
+            Console.WriteLine("\nDO WHILE loops run a number of times that depend on the condition being met, but will always run at least once.");
+
+            // foreach loops
+            Console.WriteLine("\nFOREACH loops run a number of times that depend on the amount of content.");
+            Console.WriteLine("Largely won't be looking at these until we have covered arrays.");
+
+            Console.ReadLine();
+        }
+    }
+}
