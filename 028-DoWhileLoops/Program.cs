66fa05bb1cd4f33f0c8e9f63c5ab1432e04e38cf
MrSarno <email@sarno.dev>
2022-11-03 21:19:43 +0000

    chore: add 028

---
+++ 028-DoWhileLoops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DoWhileLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            do
+            {
+                Console.WriteLine(counter);
+                counter++;
+            } while (counter < 5);
+
+
+            Console.ReadLine();
+        }
+    }
+}
