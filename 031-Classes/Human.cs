ccc695263b4fb087dbe07f43eb67e6986ea2a0d4
MrSarno <email@sarno.dev>
2022-11-04 23:28:04 +0000

    chore: add 031

---
+++ 031-Classes/Human.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Classes
+{
+	// this class is a blueprint for a datatype
+	public class Human
+	{
+		// member variable
+		public string firstName = "still waiting for a name"; // seems like as good a PH as any!
+
+		// member method
+		public void IntroduceMyself()
+		{
+			Console.WriteLine("\nHi, I'm {0}.", firstName);
+		}
+	}
+}
---
+++ 031-Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human john = new Human();
+
+            // acces public variable from outside, and even change it
+            john.firstName = "John";
+            john.IntroduceMyself();
+
+            Human andrew = new Human();
+            andrew.IntroduceMyself();
+            // I haven't set andrew.firstName
+
+            Console.ReadLine();
+        }
+    }
+}
