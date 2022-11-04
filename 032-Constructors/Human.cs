e1a04ad4fe13dc7344c1a589e4ac5b08662ad571
MrSarno <email@sarno.dev>
2022-11-04 23:49:50 +0000

    chore: add 032

---
+++ 032-Constructors/Human.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Constructors
+{
+    public class Human
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string eyeColour;
+
+        // constructor
+        public Human(string firstName, string lastName, int age, string eyeColour)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.eyeColour = eyeColour;
+        }
+
+        public void IntroduceMyself()
+        {
+            if (age > 5) // babies don't talk!
+            {
+                Console.WriteLine("Hi, I'm {0} {1}.", firstName, lastName);
+                Console.WriteLine("I am {0} years old and I have {1} eyes.", age, eyeColour);
+            }
+        }
+    }
+}
---
+++ 032-Constructors/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human john = new Human("John", "Sweeney", 30, "green");
+            john.IntroduceMyself();
+
+            Console.ReadLine();
+        }
+    }
+}
