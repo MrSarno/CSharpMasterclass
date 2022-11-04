19fa7bcd70db57c5ebd0fb039e9acbb0bbef20a3
MrSarno <email@sarno.dev>
2022-11-04 23:59:50 +0000

    chore: add 033

---
+++ 033-MultipleConstructors/Human.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace MultipleConstructors
+{
+    public class Human
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string eyeColour;
+
+        // default constructor
+        public Human()
+        {
+            Console.WriteLine("\n[Constructor called. Object has been created.]");
+        }
+
+        public Human(string firstName, string lastName, string eyeColour)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColour = eyeColour;
+        }
+
+        // parameterised constructor
+        public Human(string firstName, string lastName, int age, string eyeColour)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.eyeColour = eyeColour;
+        }
+
+        public void IntroduceMyself()
+        {
+            if (age != 0)
+            {
+                Console.WriteLine("\nHi, I'm {0} {1}.", firstName, lastName);
+                Console.WriteLine("I am {0} years old and I have {1} eyes.", age, eyeColour);
+            }
+            else
+            {
+                Console.WriteLine("\nHi, I'm {0} {1}, and I have {2} eyes.", firstName, lastName, eyeColour);
+            }
+        }
+    }
+}
---
+++ 033-MultipleConstructors/Program.cs
@@ -0,0 +1,22 @@
+﻿using MultipleConstructors;
+using System;
+
+namespace MultipleConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human john = new Human("John", "Sweeney", 30, "green");
+            john.IntroduceMyself();
+
+            Human basic = new Human();
+            basic.IntroduceMyself();
+
+            Human steve = new Human("Steve", "Stevenson", "brown");
+            steve.IntroduceMyself();
+
+            Console.ReadLine();
+        }
+    }
+}
