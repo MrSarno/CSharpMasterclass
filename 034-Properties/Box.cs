aea196894df6e7eb9da2134c33a139c86ccc611f
MrSarno <email@sarno.dev>
2022-11-17 18:10:36 +0000

    DEV: add 034-Properties

---
+++ 034-Properties/Box.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Properties
+{
+    internal class Box
+    {
+        private int length = 3;
+        private int height;
+        // private int width = -1;
+        private int volume;
+
+        public int Width { get; set; }
+
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    height = -value;
+                }
+                else
+                {
+                    height = value;
+                }
+            }
+        }
+
+        public void SetLength(int length)
+        {
+            this.length = length;
+        }
+        public int GetLength()
+        {
+            return this.length;
+        }
+
+        public int GetVolume()
+        {
+            if (length < 0)
+            {
+                throw new Exception("Values provided should not be lower than 0.");
+            }
+            else
+            {
+                return this.length * this.height * this.Width;
+            }
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Length is {0}, height is going to be {1}, and width is {2}." +
+                " This means the volume is {3}.", length, height, Width, volume = length * height * Width);
+        }
+    }
+}
---
+++ 034-Properties/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+
+            box.Height = -4;
+            box.Width = 5;
+
+            Console.WriteLine("The volume of the box is " + box.GetVolume() + ".");
+            Console.ReadLine();
+        }
+    }
+}
