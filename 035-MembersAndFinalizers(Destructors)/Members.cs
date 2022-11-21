4c7f85253d5c4cda3329d2c400e14523810c6b34
MrSarno <email@sarno.dev>
2022-11-21 12:29:46 +0000

    DEV: add 035 Members and Finalizers

---
+++ 035-MembersAndFinalizers(Destructors)/Members.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembersAndFinalizers
+{
+    internal class Members
+    {
+        // member - private fields
+        private string memberName;
+        private string jobTitle;
+        private int salary;
+
+        // member - public field
+        public int age;
+
+        // member - property - exposes jobTitle safely
+        // properties start with a capital letter!
+        public string JobTitle
+        {
+            get
+            {
+                return jobTitle;
+            }
+            set
+            {
+                jobTitle = value;
+            }
+        }
+
+        // public member Method - can be called from other classes
+        public void Introducing(bool isFriend)
+        {
+            if (isFriend)
+            {
+                SharingPrivateInfo();
+            }
+            else
+            {
+                Console.WriteLine("Hi, my name is {0}, and I am a {1}. I am {2} years old.", memberName, jobTitle, age);
+            }
+        }
+
+        private void SharingPrivateInfo()
+        {
+            Console.WriteLine("My salary is {0}.", salary);
+        }
+
+        // member - constructor
+        public Members()
+        {
+            age = 45;
+            memberName = "Stephen";
+            salary = 60000;
+            jobTitle = "Developer";
+            Console.WriteLine("[Object created.]");
+        }
+
+        // member - finalizer (previously known as destructor)
+        ~Members()
+        {
+            // cleanup statements
+            Console.WriteLine("[Deconstruction of Members object.]");
+            // only have one of these if it is important
+            // empty finalizers are bad for performance
+            Debug.Write("[DEBUG] Destruction of Members object.");
+        }
+    }
+}
---
+++ 035-MembersAndFinalizers(Destructors)/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MembersAndFinalizers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Members member1 = new Members();
+            member1.Introducing(true);
+            Console.ReadLine();
+        }
+    }
+}
