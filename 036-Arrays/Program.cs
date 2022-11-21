72daa44ff53139795d31d6240d7fa0f2b39e3f54
MrSarno <email@sarno.dev>
2022-11-21 12:41:04 +0000

    DEV: add 036 Arrays

---
+++ 036-Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Arrays store a fixed-size sequential collection of elements.");
+            Console.WriteLine("Elements must all be of the same type.");
+            Console.WriteLine("Arrays can be of all different kinds of types; ints, strings, objects, etc.");
+            Console.WriteLine("Arrays are great for storing a collection of data. It is easier to think of a collection of variables of the same type stored at contiguous memory locations.");
+            Console.WriteLine("Array indexes begin at 0, incrementing by one each time.");
+
+            // declaring an array
+            // dataType[] arrayName;
+            // e.g. int[] grades;
+
+            // initialising an array
+            // dataType[] arrayName = new dataType[amountOfEntries];
+            int[] grades = new int[5];
+
+            // assigning values to an array
+            // arrayName[index] = value;
+            grades[0] = 39;
+            grades[1] = 57;
+
+            Console.ReadLine();
+        }
+    }
+}
