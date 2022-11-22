b5e09b4f36809795ad02186f2984fd88ddd39fa7
MrSarno <email@sarno.dev>
2022-11-22 20:16:35 +0000

    DEV: add 038 Foreach Loops

---
+++ 038-ForeachLoops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ForeachLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                nums[i] = i * 10;
+            }
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                Console.WriteLine("Element {0} is {1}.", j, nums[j]);
+            }
+
+            Console.WriteLine("\n");
+            int counter = 0;
+            foreach (int k in nums) // data type is same as array
+            {
+                Console.WriteLine("Element {0} is (still) {1}.", counter, k);
+                counter++;
+            } // for loops tend to be faster than foreach loops
+
+            Console.ReadLine();
+        }
+    }
+}
