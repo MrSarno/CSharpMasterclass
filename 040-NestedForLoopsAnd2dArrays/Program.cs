749d94dc632d7387984e2ea71bacc4c5e8e63d25
MrSarno <email@sarno.dev>
2022-11-26 20:50:36 +0000

    DEV: add 040 Nested For Loops and 2D Arrays

---
+++ 040-NestedForLoopsAnd2dArrays/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace NestedForLoopsAnd2dArrays
+{
+    class Program
+    {
+        static int[,] matrix =
+        {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Using a Foreach loop;\n");
+            foreach (int item in matrix)
+            {
+                Console.Write(item + " "); // Console.WriteLine would put each element on its own line
+            }
+            
+            Console.WriteLine("\n\nUsing nested For loops;\n");
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+            }
+
+            Console.WriteLine("\n\nAnd now we'll modify the array;\n");
+
+            for (int i = 0; i<matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Random rnd = new Random();
+                    int random_int = rnd.Next(1,225);
+                    matrix[i, j] = random_int;
+                    Console.Write(matrix[i, i] + " ");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
