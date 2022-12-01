697292fdd99f08ea5d1f288c954e4fbe9f1827e2
MrSarno <email@sarno.dev>
2022-12-01 21:45:57 +0000

    DEV: add 046 Lists

---
+++ 046-Lists/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 5, 10, 15, 20, 25, 30, 35, 40 };
+
+            foreach (int element in numbers)            // foreach loop
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("\n\n--\n\n");
+
+            for (int i = 0; i < numbers.Count; i++)     // for loop
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
